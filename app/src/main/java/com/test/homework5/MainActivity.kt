ee46e0743f1c30dff50a5fea32848b2b62f3c37b
Largik <57515916+Largik@users.noreply.github.com>
2022-03-15 01:57:55 +0400

    implemented fragments via navigation

--- app/src/main/java/com/test/homework5/MainActivity.kt
+++ app/src/main/java/com/test/homework5/MainActivity.kt
@@ -3,10 +3,10 @@ package com.test.homework5
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ReelsActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.reels)
+
         supportActionBar?.hide()
     }
 }
